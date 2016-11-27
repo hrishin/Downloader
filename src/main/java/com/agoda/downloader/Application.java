3a34d5507553a57d532085263f9d8e83476034cd
hriships <hrishishinde.p@gmail.com>
2016-11-27 17:29:19 +0530

    added base

---
+++ src/main/java/com/agoda/downloader/Application.java
@@ -0,0 +1,8 @@
+package com.agoda.downloader;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/test/java/com/agoda/downloader/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.agoda.downloader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
