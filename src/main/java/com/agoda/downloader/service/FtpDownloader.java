8f817a6af3d3864318f1c032cd4a583dcd904938
hriships <hrishishinde.p@gmail.com>
2016-11-30 22:25:31 +0530

    Added ftp downloader

---
+++ src/main/java/com/agoda/downloader/service/FtpDownloader.java
@@ -0,0 +1,105 @@
+package com.agoda.downloader.service;
+
+import com.agoda.downloader.domain.DownloadState;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.io.*;
+
+/**
+ * Created by hrishikeshshinde on 30/11/16.
+ */
+public class FtpDownloader implements Downloader {
+
+    public static final int DEFAULT_PORT = 21;
+
+    private String server;
+    private int port;
+    private String filePath;
+    private String userName;
+    private String password;
+    private volatile DownloadState downloadState;
+
+    public FtpDownloader() {
+        this.downloadState = DownloadState.INITIAL;
+    }
+
+    @Override
+    public DownloadState download(String source, String path, String fileName) throws IOException {
+        processSourceURL(source);
+        FTPClient ftpClient = configuredFTPClient();
+
+        FTPFile[] files = ftpClient.listFiles(filePath);
+        if(files.length <=  0) {
+            this.downloadState = DownloadState.FAILED;
+        } else {
+            downloadFile(path, fileName, ftpClient);
+        }
+
+        return this.downloadState;
+    }
+
+    private void downloadFile(String path, String fileName, FTPClient ftpClient) throws IOException {
+        File downloadFile = new File(path + fileName);
+        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(downloadFile))) {
+            boolean success = ftpClient.retrieveFile(this.filePath, outputStream);
+            if(success) {
+                this.downloadState = DownloadState.COMPLETED;
+            } else {
+                this.downloadState = DownloadState.FAILED;
+            }
+        }
+    }
+
+    private FTPClient configuredFTPClient() throws IOException {
+        FTPClient ftpClient = new FTPClient();
+        ftpClient.connect(this.server, this.port);
+        ftpClient.enterLocalPassiveMode();
+        ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+        confgFTPCredentials(ftpClient);
+        return ftpClient;
+    }
+
+    private void confgFTPCredentials(FTPClient ftpClient) throws IOException {
+        if(userName != null && password != null) {
+            ftpClient.login(this.userName, this.password);
+        }
+    }
+
+    private void processSourceURL(String source) {
+        if(source.contains("@")) {
+            String[] sourceData = source.split("@");
+            processCredentials(sourceData[0].split("//")[1]);
+            processPath("//" + sourceData[1]);
+        } else {
+            processCredentials(source);
+        }
+    }
+
+    private void processCredentials(String userdata) {
+        String[] credentials = userdata.split(":");
+        this.userName = credentials[0];
+        this.password = credentials[1];
+    }
+
+    private void processPath(String pathData) {
+        String sourceData = pathData.split("//")[1];
+        if(sourceData.split(":").length == 2) {
+            String[] hostPort = sourceData.split(":");
+            this.port = Integer.parseInt(hostPort[1]);
+            this.server = hostPort[0];
+            this.filePath = sourceData.substring(sourceData.indexOf("/"), sourceData.length());
+        } else {
+            this.port = DEFAULT_PORT;
+            this.server = sourceData.substring(0, sourceData.indexOf("/"));
+            this.filePath = sourceData.substring(sourceData.indexOf("/"), sourceData.length());
+        }
+    }
+
+    @Override
+    public DownloadState getStatus() {
+        return null;
+    }
+}
--- src/test/java/com/agoda/downloader/service/DownloaderTest.java
+++ src/test/java/com/agoda/downloader/service/DownloaderTest.java
@@ -2,12 +2,16 @@ package com.agoda.downloader.service;
 
 import com.agoda.downloader.domain.FileResource;
 import com.agoda.downloader.domain.DownloadState;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertTrue;
 
@@ -41,15 +45,27 @@ public class DownloaderTest {
     }
 
     @Test
-    @Ignore
     public void fromFTP() throws IOException {
-        String source = "ftp://ftp.hq.nasa.gov/pub/astrophysics/FTP_Instructions.txt";
+        String source = "ftp://anonymous:myemailname@ftp.hq.nasa.gov/pub/astrophysics/FTP_Instructions.txt";
         String path = "/Users/hrishikeshshinde/";
         FileResource fileResource =  new FileResource(source);
         String fileName = fileResource.getFilename();
 
-        Downloader downloader = new HttpDownloader();
+        Downloader downloader = new FtpDownloader();
         DownloadState status = downloader.download(source, path, fileName);
         assertTrue(status == DownloadState.COMPLETED);
+    }
+
+    @Test
+    @Ignore
+    public void getPath() {
+        String test = "ftp://anonymous:myemailname@ftp.hq.nasa.gov/pub/astrophysics/FTP_Instructions.txt";
+        String[] test1 = test.split("@");
+        String userName = test1[0].split("//")[1];
+        String password = test1[1];
+        String[] tests = test.split("//");
+        String server = tests[1].substring(0, tests[1].indexOf("/"));
+        String file = tests[1].substring(tests[1].indexOf("/"), tests[1].length());
+        System.out.print(server + ": " + file);
     }
 }
