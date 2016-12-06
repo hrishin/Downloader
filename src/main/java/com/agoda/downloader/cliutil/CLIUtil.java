0b7057b0a2bb34a17aef8ed54c71d58afb51fa6a
hriships <hrishishinde.p@gmail.com>
2016-12-06 11:06:12 +0530

    Replaced string literals with constants and updated logger level

--- src/main/java/com/agoda/downloader/cliutil/CLIUtil.java
+++ src/main/java/com/agoda/downloader/cliutil/CLIUtil.java
@@ -6,6 +6,10 @@ import org.apache.commons.cli.*;
  * Utility class to initialize, extract CLI options
  */
 public class CLIUtil {
+    public static final String DOWNLOAD_HELP = "Download Help";
+    public static final String DOWNLOAD_SOURCE_DESCRIPTION = "Download data sources in csv form";
+    private static final String DOWNLOAD_PATH_DESCRIPTION = "Local download path";
+    public static final String HELP_DESCRIPTION = "Help";
     private static CommandLineParser parser;
     private static CommandLine commandLine;
     private static Options options;
@@ -18,10 +22,10 @@ public class CLIUtil {
      */
     public static void initOptions(String[] args) throws ParseException {
         Option firstNDataOption = new Option(CLIOptions.DOWNLOAD_LOCATION_SS.toString(), CLIOptions.DOWNLOAD_LOCATION_LS.toString(),
-                                            true, "Local download path");
-        Option inputDataOption  = new Option(CLIOptions.DOWNLOAD_SOURCE_SS.toString(),
-                                            CLIOptions.DOWNLOAD_SOURCE_LS.toString(), true, "Downlaod data sources in csv form");
-        Option helpOption 		= new Option(CLIOptions.HELP_SS.toString(), "Help");
+                                            true, DOWNLOAD_PATH_DESCRIPTION);
+        Option inputDataOption  = new Option(CLIOptions.DOWNLOAD_SOURCE_SS.toString(), CLIOptions.DOWNLOAD_SOURCE_LS.toString(),
+                                            true, DOWNLOAD_SOURCE_DESCRIPTION);
+        Option helpOption 		= new Option(CLIOptions.HELP_SS.toString(), HELP_DESCRIPTION);
 
         options = new Options();
         options.addOption(firstNDataOption);
@@ -63,7 +67,7 @@ public class CLIUtil {
      */
     public static void help() {
         HelpFormatter formater = new HelpFormatter();
-        formater.printHelp("Download Help", options);
+        formater.printHelp(DOWNLOAD_HELP, options);
         System.exit(0);
 
     }
